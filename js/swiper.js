a20e735cdae3210724ebf5120397d8ce5ba07391
puritechlab1155 <pub@puritechlab.com>
2025-05-27 19:59:45 +0900

    sub07 edit

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+// 스와이퍼 조작
+        document.addEventListener("DOMContentLoaded", function () {
+            new Swiper(".mySwiper", {
+            loop: true,                  // 무한 루프
+            navigation: {
+                nextEl: ".swiper-button-next",  // 다음 버튼
+                prevEl: ".swiper-button-prev",  // 이전 버튼
+            },
+            autoplay: {
+                delay: 3000,                   // 자동 재생 (선택)
+                disableOnInteraction: false,  // 유저 조작 후에도 계속
+            },
+            touchRatio: 1,                  // 터치 감도 (모바일 지원 기본값)
+            grabCursor: true,               // 마우스 커서가 손모양으로
+            });
+        });
